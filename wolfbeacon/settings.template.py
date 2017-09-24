b041d68d3da67f60a37bda6ec82960c7e6c5a05e
bholagabbar <dev.shreyans96@gmail.com>
2017-09-25 01:47:36 +0530

    Updated settings-template

--- wolfbeacon/settings.template.py
+++ wolfbeacon/settings.template.py
@@ -45,7 +45,6 @@ MIDDLEWARE = [
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     )
 }
 
