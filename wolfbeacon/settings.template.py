9199bf73d7bfe795fdc72094a9c0940094b45ddd
bholagabbar <dev.shreyans96@gmail.com>
2017-11-20 13:52:04 +0530

    Updated static files location

--- wolfbeacon/settings.template.py
+++ wolfbeacon/settings.template.py
@@ -115,10 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/code/static/'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 # Additional Security Settings
 # python manage.py check --deploy
